18f37e7f399a90e4935eefcd58631789d7491b83
Daniszz <hritcudenis03@gmail.com>
2024-02-27 13:25:07 +0200

    Update Lab1.java

--- Lab1/src/main/java/com/mycompany/lab1/Lab1.java
+++ Lab1/src/main/java/com/mycompany/lab1/Lab1.java
@@ -16,11 +16,14 @@ lab1.compulsory();
  }
  void compulsory() {
      System.out.println("Hello World!");
-        String languages[]={"C", "C++", "C#", "Python", "Go", "Rust", "JavaScript", "PHP", "Swift", "Java"};
+        String[] languages={"C", "C++", "C#", "Python", "Go", "Rust", "JavaScript", "PHP", "Swift", "Java"};
           int n = (int) (Math.random() * 1_000_000);
           int result= n*3;
-          result=result+0b10101;
-          result=result+0xFF;
+          String binar="10101";
+          String hexadecimal="FF";
+          int cifra1=Integer.parseInt(binar,2);
+          int cifra2=Integer.parseInt(hexadecimal,16);
+          result=result+cifra1+cifra2;
           result=result*6;
           while(result>9)
           {
